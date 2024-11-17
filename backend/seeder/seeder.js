6f39be1bdc8ffe750c62e91357d2f28bca3d4c28
mariamarcela <maria.marcela.sousa@gmail.com>
2024-11-17 14:54:24 -0300

    deploy

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -5,7 +5,7 @@ import Product from "../models/product.js";
 const seedProducts = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
+      "mongodb+srv://facef_marcela:%25N%2C%23%5E6TqgVgT%3Bwx@cluster0.xwpjt.mongodb.net/shopit-v2?retryWrites=true&w=majority&appName=Cluster0"
     );
 
     await Product.deleteMany();
